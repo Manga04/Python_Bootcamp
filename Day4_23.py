18441ca54a659ba1a93216c9db6a5a980c46a7f7
Manga <128495218+Manga04@users.noreply.github.com>
2024-07-24 14:30:33 +0530

    Day 4

---
+++ Day4_23.py
@@ -0,0 +1,15 @@
+#sort elements: 1st half in ascending and 2nd half in decending
+my_list=list(map(int, input().split()))
+middle=len(my_list)//2
+first=my_list[:middle]
+second=my_list[middle:]
+for i in range(len(first)):
+    for j in range(i+1,len(first)):
+        if first[i]>first[j]:
+            first[i],first[j]=first[j],first[i]
+for i in range(len(second)):
+    for j in range(i+1,len(second)):
+        if second[i]<second[j]:
+            second[i],second[j]=second[j],second[i]
+result=first+second
+print(result)
